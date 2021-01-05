0829e31168f8ce51bd45baecdd39413f830bff04
Antman18010 <75477347+Antman18010@users.noreply.github.com>
2021-01-05 18:26:58 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,41 @@
-var wall, thickness;
-var bullet, speed, weight;
-
-function setup(){
-  createCanvas(1600,400);
- speed = random(55,90);
- thickness = random(232,321);
- weight = random(30,52);
- bullet = createSprite(50,200,50,30);
- wall=createSprite(1500,200,thickness,height/2);
+var bullet,wall;
+var speed,weight,thickness;
+function setup() {
+  createCanvas(1200,800);
+  speed= random(223,321)
+  weight=random(30,52)
+  thickness=random(22,83)
+  wall = createSprite(1000, 400, thickness,height/2);
+  bullet = createSprite(50, 400, 80, 20);
 }
 
-function draw(){
-  background(0,0,0);
-  bullet.velocityX = speed;
-  if(wall.x-bullet.x < (bullet.width+wall.width)/2){
-    bullet.velocityX=0;
+function draw() {
+  background(0); 
+  bullet.velocityX=speed;
+  bullet.shapeColor=color("white");
+  if(hasCollided(bullet,wall)){
+    
     var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
     if(damage>10){
       wall.shapeColor=color(255,0,0);
     }
+
     if(damage<10){
       wall.shapeColor=color(0,255,0);
     }
+    bullet.velocityX= 0;
   }
-    drawSprites();
+
+
+  drawSprites();
+}
+
+function hasCollided(Lbullet,Lwall)
+{
+  var bulletRightEdge=Lbullet.x+Lbullet.width;
+  var  wallLeftEdge=Lwall.x;
+if(bulletRightEdge>=wallLeftEdge){
+  return true
+}
+return false;
 }
